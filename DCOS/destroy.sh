bfa1678ed8542c0f384c88604b5116f4d0e20949
slombardo2 <60935040+slombardo2@users.noreply.github.com>
2021-02-28 14:14:25 -0500

    Update destroy.sh

--- DCOS/destroy.sh
+++ DCOS/destroy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-dcos marathon app remove robotshop/cart     
-dcos marathon app remove robotshop/catalogue
-dcos marathon app remove robotshop/dispatch 
-dcos marathon app remove robotshop/mongodb  
-dcos marathon app remove robotshop/mysql    
-dcos marathon app remove robotshop/payment  
-dcos marathon app remove robotshop/rabbitmq 
-dcos marathon app remove robotshop/redis    
-dcos marathon app remove robotshop/shipping 
-dcos marathon app remove robotshop/user     
-dcos marathon app remove robotshop/web      
+dcos marathon app remove slombardo2/cart     
+dcos marathon app remove slombardo2/catalogue
+dcos marathon app remove slombardo2/dispatch 
+dcos marathon app remove slombardo2/mongodb  
+dcos marathon app remove slombardo2/mysql    
+dcos marathon app remove slombardo2/payment  
+dcos marathon app remove slombardo2/rabbitmq 
+dcos marathon app remove slombardo2/redis    
+dcos marathon app remove slombardo2/shipping 
+dcos marathon app remove slombardo2/user     
+dcos marathon app remove slombardo2/web      
